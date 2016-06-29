9270913ef8d6f21b317565509092f5cbc68646b0
Arif <abbu.arif@gmail.com>
2016-06-29 14:13:15 +0530

    Update getaccess.py

--- getaccess.py
+++ getaccess.py
@@ -1,4 +1,4 @@
 from sherpa import cli_wrapper as cli
-accountinfo = cli.clients.scalerim.accountCreate('','user1','user1@hgst.com')
+accountinfo = cli.clients.scalerim.accountCreate('','user1','user1@test.com')
 apikeyinfo = cli.clients.scalerim.apiKeyGenerate('','%s' % accountinfo.canonicalId)
 print 'accessKey=%s, secretKey=%s' %(apikeyinfo.accessKey, apikeyinfo.secretKey)
