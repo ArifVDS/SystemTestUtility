87b5f57daa52e35d769d27a24decd4f9e1b04476
Arif <abbu.arif@gmail.com>
2016-06-29 14:11:40 +0530

    Update accesskeygenerator.py

--- accesskeygenerator.py
+++ accesskeygenerator.py
@@ -8,15 +8,15 @@ pwd.strip()
 
 class KeyGenerator():
 
-    def run(self, user, scalerip, loginuser='marvin', loginpass='marvin', getaccessfile = 'getaccess.py'):
+    def run(self, user, systemip, loginuser='<username>', loginpass='<password>', getaccessfile = 'getaccess.py'):
 
         self._COMMON_SSH_OPTIONS = "-o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -o ConnectTimeout=5"
         self.user = user
-        self.scalerip = scalerip
+        self.systemip = systemip
         self.loginuser = loginuser
         self.loginpass = loginpass
         self.getaccessfile = '%s/%s' %(pwd, getaccessfile)
-        print "INFO\nUSER:%s\nscalerIP:%s\n" %(self.user, self.scalerip)
+        print "INFO\nUSER:%s\systemip:%s\n" %(self.user, self.systemip)
         return self.getaccessrights()
 
     def execute_command(self, command):
@@ -27,9 +27,9 @@ class KeyGenerator():
 
         with open(self.getaccessfile, "a") as f:
             f.write('from sherpa import cli_wrapper as cli\n')
-            f.write("accountinfo = cli.clients.scalerim.accountCreate('','%s',"
+            f.write("accountinfo = cli.clients.systemip.accountCreate('','%s',"
                     "'%s@hgst.com')\n" %(self.user, self.user))
-            f.write("apikeyinfo = cli.clients.scalerim.apiKeyGenerate('','%s' "
+            f.write("apikeyinfo = cli.clients.systemip.apiKeyGenerate('','%s' "
                     "% accountinfo.canonicalId)\n")
             f.write("print 'accessKey=%s, secretKey=%s' %(apikeyinfo.accessKey, apikeyinfo.secretKey)\n")
         f.close()
@@ -39,26 +39,26 @@ class KeyGenerator():
         if not pkg_status:
             self.execute_command('rpm -ivh %s/bin/%s' %(pwd, 'sshpass-1.05-9.1.x86_64.rpm'))
             time.sleep(10)
-        print('Send the script [%s] to the scaler [%s] for sherpa command execution'
-              '.' %(self.getaccessfile, self.scalerip))
+        print('Send the script [%s] to the systemip [%s] for sherpa command execution'
+              '.' %(self.getaccessfile, self.systemip))
         cmd = 'sshpass -p %s scp %s %s %s@%s:/home/%s' %(self.loginpass,
                                                          self._COMMON_SSH_OPTIONS,
                                                          self.getaccessfile, self.loginuser,
-                                                         self.scalerip, self.loginuser)
+                                                         self.systemip, self.loginuser)
         self.execute_command(cmd)
         print self.getaccessfile
         os.remove(self.getaccessfile)
-        print('Execute the script [%s] on the scaler [%s] to generate the '
-              'keys' %(self.getaccessfile, self.scalerip))
+        print('Execute the script [%s] on the systemip [%s] to generate the '
+              'keys' %(self.getaccessfile, self.systemip))
         cmd1 = 'sshpass -p %s ssh -o StrictHostKeyChecking=no %s@%s "source ' \
                '/opt/ampli/apps/sherpa/venv/bin/activate; python getaccess.py"' % (self.loginpass,
                                                                          self.loginuser,
-                                                                         self.scalerip)
+                                                                         self.systemip)
         access_results = self.execute_command(cmd1).strip()
         if not access_results:
-            print('User already exist for the scaler node. Please specify a different user for test run.')
+            print('User already exist for the systemip node. Please specify a different user for test run.')
             sys.exit(0)
         else:
-            print('User successfully created for the scaler node. Proceed with test execution with :[%s]' %(access_results))
+            print('User successfully created for the systemip node. Proceed with test execution with :[%s]' %(access_results))
             resultdict = dict(item.split("=") for item in access_results.split(", "))
             return resultdict
